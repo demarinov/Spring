ea81a32d3573578afa62616b012b3c88cb6ef449
dm <methodical@abv.bg>
2020-03-05 00:29:52 +0200

    minor change

---
+++ spring-country-tour/src/test/java/com/dido/springboot/demo/countrytourapp/CountryTourAppTests.java
@@ -0,0 +1,13 @@
+package com.dido.springboot.demo.countrytourapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryTourAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
